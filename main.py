e40cd1cd0a8b29dd1b0d87642bb29f3443881fe8
BlacF0X <c.mertens@students.ephec.be>
2024-12-06 20:14:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-from fraction import Fraction
-import unittest
-
-
-fraction1 = Fraction(15,8)
+from fraction import *
+fraction1 = Fraction(1,4)
 fraction2 = Fraction(0,6)
-fraction3 = Fraction(84,12)
+fraction3 = Fraction(2,3)
+fraction4 = Fraction(50,30)
+def testing():
+    print('print')
+    print(fraction1)
+    print(fraction2)
+    print(fraction3)
+    print(fraction4)
+    print('as_mixed_number')
+    print(fraction1.as_mixed_number())
+    print('+')
+    print(fraction1+fraction3)
+    print('-')
+    print(fraction4-fraction3)
+    print('*')
+    print(fraction1*fraction2)
+    print('/')
+    print(fraction2/fraction1)
+    print('** + float')
+    print(fraction3**fraction1)
+    print((2/3)**(1/4))
+    print('==')
+    print(fraction1 == fraction2)
+    print(fraction1 == Fraction(1,4))
+    print('--zero--')
+    print(fraction1.is_zero())
+    print(fraction2.is_zero())
+    print('--integer--')
+    print(Fraction(8,2).is_integer())
+    print('--proper--')
+    print(fraction1.is_proper())
+    print('--unit--')
+    print(fraction2.is_unit())
+    print(Fraction(4,4).is_unit())
+    print('--adjacent--')
+    print(fraction3.is_adjacent_to(fraction4))
 
-
-class Fract_test(unittest.TestCase):
-    def test_fraction_den_positif(self):
-        """Vérification du calcul de la fraction dans le cas den>0"""
-        self.assertEqual(fraction1-fraction2, Fraction(15,8),"f1 - f2")
-        self.assertEqual(fraction2.is_zero(), True, "f2 is 0")
-        self.assertEqual(fraction1 + fraction3, Fraction(71,8), "f1+f3")
-
-    def test_fraction_den_zero(self):
-        """Vérification de la positivité du denominateur"""
-        self.assertRaises(AssertionError,Fraction,15,0)
-
-    def test_fraction_not_int(self):
-        """Vérification de la génération d'une exception en cas d'appel sur un nombre négatif"""
-        self.assertRaises(TypeError, Fraction, None,True)
-        self.assertRaises(TypeError, Fraction, 'huit',[15,23])
-
+if __name__ == '__main__':
+    testing()
